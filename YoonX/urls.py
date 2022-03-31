d2399360515a9cddb4ad6770d7f9345159bbf51e
YoonX <salescloud52@gmail.com>
2022-03-31 16:26:20 +0900

    대문 페이지와 자기소개 페이지 만들기

--- YoonX/urls.py
+++ YoonX/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('blog/', include('blog.urls'))
     path('admin/', admin.site.urls),
+    path('', include('single_pages.urls')),
+
 ]
